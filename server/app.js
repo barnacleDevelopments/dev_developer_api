52b23544a9741baf834c5bd077698cd1cc97e059
Devin <ddavis@immediac.com>
2021-08-31 14:12:39 -0300

    Feature Update: Cors headers whitelist

--- server/app.js
+++ server/app.js
@@ -43,9 +43,18 @@ db.once('open', function () {
 // +++++++++++++++
 
 // CROSS ORGIN REQUEST SETTINGS
-app.use(cors({
-  origin: "*",
-}));
+var whitelist = ['https://blog.devdeveloper.ca', 'https://devdeveloper.ca']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
